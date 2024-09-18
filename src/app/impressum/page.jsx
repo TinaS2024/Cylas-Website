922089339e065ed407bfe1eff9c118c78d590f72
Tina Schmidtbauer <tina.schmidtbauer@bolasys.de>
2024-09-18 12:21:59 +0200

    Impressums-Daten geändert

--- src/app/impressum/page.jsx
+++ src/app/impressum/page.jsx
@@ -8,10 +8,14 @@ const Impressum = () =>
     <table width={450}>
         <h2 style={{textAlign:"left"}}>Anschrift</h2>
         <tbody>
-            <tr><td><span>Cylas AG</span></td></tr>
+        <tr><td><span>Cylas AG Büro:</span></td></tr>
+            <tr><td>Fritz-Wagner-Siedlung 16</td></tr>
+            <tr><td>09116 Chemnitz </td></tr> 
+            <tr><td><b>Tel.:</b> (03 71) 3 35 06 67</td></tr>
+            <tr><td style={{height:"25px"}}></td></tr>
+            <tr><td><span>Cylas AG Werkstatt:</span></td></tr>
             <tr><td>Straßburger Straße 7</td></tr>
             <tr><td>09120 Chemnitz </td></tr>
-            <tr><td><b>Tel.:</b> (03 71) 3 35 06 67</td></tr>
             <tr><td style={{height:"50px"}}><b>Geschäftsführer:</b></td></tr>
             <tr><td><span>Reiner Boes:</span> &emsp;&emsp;(01 71) 8 50 09 08</td></tr>
             <tr><td><span>Dr. Gerd Pfeifer:</span> &ensp;(01 60) 96 24 85 44 </td></tr>
