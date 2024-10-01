9b5c28bd0cf94f782d52282a4e4e50b31bc8005f
Tina Schmidtbauer <tina.schmidtbauer@bolasys.de>
2024-10-01 12:41:58 +0200

    E-Mail-Provider geändert

--- src/app/api/kontakt/route.js
+++ src/app/api/kontakt/route.js
@@ -5,21 +5,21 @@ export async function POST(req)
     const {username,email,bereich,nachricht} = await req.json() 
     
     const transporter = nodemailer.createTransport({
-            host:"smtp-mail.outlook.com",
-            service: "Outlook",
-            port: 587,
+            host: "smtp.strato.de",//"smtp-mail.outlook.com"
+            service: "Strato", //"Outlook"
+            port: 465, //587
             secureConnection: false,
             tls: {
                 ciphers: "SSLv3"
             },
             auth: {
-                user:"bolasys_client@outlook.de" ,//process.env.EMAIL_USER,
-                pass:"kZ4xj8vkVABf9WB" ,//process.env.EMAIL_PASS,
+                user:"otrs@cylas.de",//"bolasys_client@outlook.de" 
+                pass:"42lasyc-WS" //"kZ4xj8vkVABf9WB" 
             }
         });   
         
     const mailData = {
-         from: "bolasys_client@outlook.de",
+         from: "otrs@cylas.de",//"bolasys_client@outlook.de",
          to: "Tina.Schmidtbauer@bolasys.de",
          subject: bereich,
          html: '<h2>Kundenanfrage an Cylas AG</h2><p>Von: '+username+'</p><p>Email-Absender: '+email+'</p><p>Nachricht: <br/> '+nachricht+'</p>',
