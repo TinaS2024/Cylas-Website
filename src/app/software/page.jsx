6685fc8b45481d34c40607cb9996f01d49034232
TinaS2024 <Tina.Schmidtbauer@bolasys.de>
2024-10-15 10:10:51 +0200

    Cylas-Software-Card geändert

--- src/app/software/page.jsx
+++ src/app/software/page.jsx
@@ -1,9 +1,5 @@
 import Punkt from "../components/Punkt.jsx";
 import Textfeld from "../components/Textfeld.jsx";
-import Image from "next/image";
-import img3 from "../image/Auftragsverwaltung.png";
-import img4 from "../image/Lagermanagement.png";
-import img5 from "../image/Verpackung.png";
 
 const Software = () =>
     {
@@ -13,36 +9,20 @@ const Software = () =>
             <h2>Zusätzliche Software für den Trommelgravierer<br/> von der Bolasys GmbH</h2>
             </div>
 
-            <div style={{marginLeft:"15%"}}>
-            <Textfeld titel="Auftragsverwaltung">
-            <div className="profile-img"> 
-                <Image src={img3} width={70} height={70} alt=""/>
-                </div>
-                <Punkt text="&nbsp;Neue Aufträge nach Vorlage" />
-                <Punkt text="&nbsp;Automatisches Erstellen von Aufträgen"/>
-                <Punkt text="&nbsp;Auftragsstatus-Monitor"/>
-                <Punkt text="&nbsp;Workflow- und Prozessauswahl"/>
-                <Punkt text="&nbsp;Datenaustausch mit externer Software"/>    
+            <div className="software-card" >
+            <Textfeld titel="Software für:">
+                <Punkt text="&nbsp;Auftragsvorbereitung & Annahme"/>
+                <Punkt text="&nbsp;Auftragsverwaltung"/>
+                <Punkt text="&nbsp;Designer"/>
+                <Punkt text="&nbsp;Nesting"/>
+                <Punkt text="&nbsp;Komissionierungsmanagement"/>    
+                <Punkt text="&nbsp;Interface für Graviermaschinen"/>
+                <Punkt text="&nbsp;Montagestation"/>
+                <Punkt text="&nbsp;Komissionierungs-& Versandmodul"/>   
+                <Punkt text="&nbsp;Externe Software"/>
+                <Punkt text="&nbsp;GPE-Basis"/>   
             </Textfeld>
 
-            <Textfeld titel="Lagermanagement">
-            <div className="profile-img"> 
-                <Image src={img4} width={70} height={70} alt=""/>
-                </div>
-                <Punkt text="&nbsp;App zur Kommissionierung"/>
-                <Punkt text="&nbsp;Erstellung von Komponentenlisten nach Gummiplatten und Lagerfach"/>
-                <Punkt text="&nbsp;Datenaustausch mit externer Warenwirschaft"/>
-            </Textfeld>
-   
-            <Textfeld titel="Montageprogramm">
-            <div className="profile-img"> 
-                <Image src={img5} width={70} height={70} alt=""/>
-                </div>
-                <Punkt text="&nbsp;Zuordnung von Textplatten und Gehäusen"/>
-                <Punkt text="&nbsp;Ausdruck der Versandpapiere"/>
-                <Punkt text="&nbsp;Berechung Verpackungsvolumen"/>
-                <Punkt text="&nbsp;Datenübergabe an Versanddienstleister"/>
-            </Textfeld>
             </div>
        
 
