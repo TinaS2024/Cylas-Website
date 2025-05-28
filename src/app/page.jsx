5a0fa5255426c66077495b2ac78e1822dec0a8ac
TinaS2024 <Tina.Schmidtbauer@bolasys.de>
2025-05-28 12:55:02 +0200

    Relief-Bilder auf Landingpage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,9 @@
 /* eslint-disable react/jsx-key */
 import gravierer from "../app/videos/video2.mp4";
 import MyVideo from "./components/MyVideo.jsx";
+import flexfire_relief from "../app/image/flexfire_relief_schief.png";
+import drumfire_relief from "../app/image/drumfire_relief_trommel.png";
+
 
 
 
@@ -10,29 +13,28 @@ const Home = () =>
 {
     return(
 
-        <div className="abstand" style={{backgroundColor:"#286464"}}> 
-
+        <div className="abstand" style={{backgroundColor:"#286464",backgroundImage:`url(${drumfire_relief.src})`,backgroundSize:"30%",backgroundRepeat:"no-repeat",backgroundPosition:"left"}}> 
+        <div style={{backgroundImage:`url(${flexfire_relief.src})`,backgroundSize:"30%",backgroundRepeat:"no-repeat",backgroundPosition:"right",zIndex:"1"}}>
         
            {/*<h2>Industrielle Produktion und Vertrieb von <br/>Lasergeräten und sonstigen physikalisch-technischen Erzeugnissen <br/>sowie Vornahme der hierzu notwendigen Investitionen <br/> in Betriebsanlagen und aller sonstigen hiermit zusammenhängenden Geschäfte.</h2>*/}
            <p className="landing_1">Entwicklung & Produktion von Lasergeräten</p>
            <p className="landing_1">Technologische Spitzenprodukte, Spezialisiert auf physikalisch-technische Lösungen</p>
            <p className="landing_1">Innovative Systemlösungen</p>
            <p className="landing_1">Modernste Betriebsanlagen</p>
-         
-    
+                   
            
-           <br />
-       <MyVideo path={gravierer} width={400} height={400}></MyVideo>
+           <MyVideo path={gravierer} width={400} height={400}></MyVideo>
+       
        <br/>
-       <p className="landing_2">Umfassendes Geschäftsfeld: <br/>Wir bieten nicht nur hochmoderne Lasertechnologie,<br/> sondern auch professionelle Maschinenwartung und maßgeschneiderte Softwarelösungen.</p>
-             
-            <div style={{marginTop:"100px"}} className="flyer">
+       <p className="landing_2">Umfassendes Geschäftsfeld: <br/>Wir bieten nicht nur hochmoderne Lasertechnologie,<br/> sondern auch professionelle Maschinenwartung und <br/>maßgeschneiderte Softwarelösungen.</p>
+
+            <div style={{marginTop:"50px"}} className="flyer">
             <a  href="flyer_ger.pdf" download target="_blank"  rel="noopener noreferrer" alt="Flyer">
             <button style={{marginTop:"-20px"}} className="button-container link-style-send abstand-unten" type="button">Flyer</button>
             </a>
             </div>
 
-            
+            </div>
         </div>
       
 
