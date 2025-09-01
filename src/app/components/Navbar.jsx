8f9e34ecaf60fddb95f34ab0f84179cfc74d3dff
TinaS2024 <Tina.Schmidtbauer@bolasys.de>
2025-09-01 14:10:23 +0200

    Test

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () =>
     <ButtonNeu button_titel="Downloads" button_href="/downloads" topR="0px" topL="0px" bottomR ="0px" bottomL="0px"/>
     <ButtonNeu button_titel="Kontakt" button_href="/kontakt" topR="15px" topL="0px" bottomR ="15px" bottomL="0px"/>
  
-  {/* <Link href="/" locale="en"><p>Englisch</p></Link>
+  {/* <Link href="/" locale="en"><p>englisch</p></Link>
    <Link href="/" locale="de"><p>Deutsch</p></Link>*/} 
    
         
